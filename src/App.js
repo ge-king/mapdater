7d3c3e7bbf515c3dca4cb633cfae9fcaaf0e163c
George <george.pking@gmail.com>
2023-11-16 00:11:42 +0000

    added globe and basic functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { NextUIProvider, Navbar,NavbarBrand, NavbarContent, NavbarItem, Link, Button, Spacer } from '@nextui-org/react';
+import {Card, CardBody} from "@nextui-org/react";
+import './App.css'
+import React, {useRef, useState, useEffect } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VvcnoiLCJhIjoiY2xwMGJ4cDhiMDl2dzJwbmxsMG81bWFlYyJ9.KBGCIDamvcpo74WyGU3NVQ'; 
+
+
+const App = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+  const headerRef = useRef(null);
+  const footerRef = useRef(null);
+
+
+  const adjustMapHeight = () => {
+    const headerHeight = headerRef.current?.offsetHeight || 0;
+    const footerHeight = footerRef.current?.offsetHeight || 0;
+    const windowHeight = window.innerHeight;
+    const mapHeight = windowHeight - headerHeight - footerHeight;
+    if (mapContainer.current) {
+      mapContainer.current.style.height = `${mapHeight}px`;
+    }
+  };
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+    container: mapContainer.current,
+    style: 'mapbox://styles/georz/clp0d043500i201qjfixs7wyu',
+    center: [lng, lat],
+    projection: 'globe',
+    zoom: 1.5
+    });
+    });
+
+
+    return (
+      <NextUIProvider>
+        <div className="container">
+        <header className="header">
+          <h1>MAPDATE</h1>
+        </header>
+          <div ref={mapContainer} className="map-container" />
+          <Card className="centered-card" isBlurred='true'>
+          <CardBody className="card-body">
+            <p className="card-text">Welcome to MapDate. Are you ready to answer some questions to figure out exactly when your map was made?</p>
+            <Spacer y={5} />
+            <Button fullWidth='false' variant="ghost" color="success" className="button-go">Go</Button>
+          </CardBody>
+        </Card>
+          <footer>
+            Made by George
+          </footer>
+        </div>
+      </NextUIProvider>
+    );
+  };
 
 export default App;
