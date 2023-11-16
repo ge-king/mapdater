02cca8ed64041df691bcd9ea73c536ada472cf36
George King <georgeking@172-26-85-196.eduroam.bath.ac.uk>
2023-11-16 15:19:20 +0000

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { NextUIProvider, Navbar,NavbarBrand, NavbarContent, NavbarItem, Link, Button, Spacer } from '@nextui-org/react';
+import { NextUIProvider, Navbar,NavbarBrand, NavbarContent, NavbarItem, Link, Button, Spacer } from "@nextui-org/react";
 import {Card, CardBody} from "@nextui-org/react";
 import './App.css'
 import React, {useRef, useState, useEffect } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VvcnoiLCJhIjoiY2xwMGJ4cDhiMDl2dzJwbmxsMG81bWFlYyJ9.KBGCIDamvcpo74WyGU3NVQ'; 
 
@@ -15,6 +17,15 @@ const App = () => {
   const [zoom, setZoom] = useState(9);
   const headerRef = useRef(null);
   const footerRef = useRef(null);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const darkStyle =  'mapbox://styles/georz/clp1avglo01ej01o4dwy2bzgw';
+const lightStyle = 'mapbox://styles/georz/clp0d043500i201qjfixs7wyu'; // Replace with your dark mode style URL
+
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
 
 
   const adjustMapHeight = () => {
@@ -31,19 +42,31 @@ const App = () => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
     container: mapContainer.current,
-    style: 'mapbox://styles/georz/clp0d043500i201qjfixs7wyu',
+    style: darkMode ? darkStyle : lightStyle,
     center: [lng, lat],
     projection: 'globe',
     zoom: 1.5
     });
     });
 
+    useEffect(() => {
+      if (map.current) {
+        map.current.setStyle(darkMode ? darkStyle : lightStyle);
+      }
+    }, [darkMode]);
+
 
     return (
       <NextUIProvider>
-        <div className="container">
+        <div className={`${darkMode ? 'dark' : 'light'} container`}>
         <header className="header">
           <h1>MAPDATE</h1>
+          <Button isIconOnly onClick={toggleDarkMode}>
+          {darkMode ? 
+      <LightModeIcon/>:  // Icon for light mode
+      <DarkModeIcon/>    // Icon for dark mode
+    }
+    </Button>
         </header>
           <div ref={mapContainer} className="map-container" />
           <Card className="centered-card" isBlurred='true'>
